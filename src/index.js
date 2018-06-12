25538fcaa334336daba71110c930d05f8f79cc45
yangzheng <yangzheng@qingting.fm>
2018-06-12 16:04:46 +0800

    Export RecordsPage and RecordPage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export App from './app';
 export ServiceManager from './services/ServiceManager';
 export Page from './pages/Page';
+export RecordPage from './pages/RecordPage';
+export RecordsPage from './pages/RecordsPage';
 
