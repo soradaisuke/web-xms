e5397fa41828bcba3a114881d26b9f5222f203c4
yangzheng <yangzheng@qingting.fm>
2019-07-23 10:28:55 +0800

    style(NoTicket): remove useless file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 export default from './app';
 
 export dynamic from 'dva/dynamic';
-export { Link } from 'react-router-dom';
-
 export Column from './schema';
 export Action from './actions';
-
 export request from './services/request';
-export Page from './pages/Page';
-export ActivatorModal from './components/ActivatorModal';
-export Tags from './components/FormItems/Tags';
-export UploadImage from './components/FormItems/UploadImage';
-export DatePickerWithPresets from './components/DatePickerWithPresets';
-export history from './utils/history';
-export textToPath from './utils/textToPath';
-export AudioPlayer from './components/Common/AudioPlayer';
-export CommonArray from './components/FormItems/CommonArray';
-export Group from './components/Group';
-export Select from './components/FormItems/Select';
+
+// export Page from './pages/Page';
+// export ActivatorModal from './components/ActivatorModal';
+// export Tags from './components/FormItems/Tags';
+// export UploadImage from './components/FormItems/UploadImage';
+// export DatePickerWithPresets from './components/DatePickerWithPresets';
+// export history from './utils/history';
+// export textToPath from './utils/textToPath';
+// export AudioPlayer from './components/Common/AudioPlayer';
+// export CommonArray from './components/FormItems/CommonArray';
+// export Group from './components/Group';
+// export Select from './components/FormItems/Select';
