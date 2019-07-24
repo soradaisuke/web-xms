9a2e48310c9f3fc39d85fcc6c788e1e557ef0ecd
yangzheng <yangzheng@qingting.fm>
2019-07-24 12:27:58 +0800

    feat(NoTicket): export history

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ export default from './app';
 export Column from './schema';
 export Action from './actions';
 export request from './services/request';
+export history from './utils/history';
 
 // export Page from './pages/Page';
 // export ActivatorModal from './components/ActivatorModal';
 // export Tags from './components/FormItems/Tags';
 // export UploadImage from './components/FormItems/UploadImage';
 // export DatePickerWithPresets from './components/DatePickerWithPresets';
-// export history from './utils/history';
 // export textToPath from './utils/textToPath';
 // export AudioPlayer from './components/Common/AudioPlayer';
 // export CommonArray from './components/FormItems/CommonArray';
