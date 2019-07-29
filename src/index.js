8e7ba50b44f1f6f9eb868d29a1cad1dbc51963b6
yangzheng <yangzheng@qingting.fm>
2019-07-29 16:35:35 +0800

    feat(NoTicket): export ActivitorModal

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ export Action from './actions';
 export request from './services/request';
 export history from './utils/history';
 export Group from './components/Group';
+export ActivatorModal from './components/ActivatorModal';
 
 // export Page from './pages/Page';
-// export ActivatorModal from './components/ActivatorModal';
 // export Tags from './components/FormItems/Tags';
 // export UploadImage from './components/FormItems/UploadImage';
 // export DatePickerWithPresets from './components/DatePickerWithPresets';
 // export textToPath from './utils/textToPath';
 // export AudioPlayer from './components/Common/AudioPlayer';
 // export CommonArray from './components/FormItems/CommonArray';
-
 // export Select from './components/FormItems/Select';
