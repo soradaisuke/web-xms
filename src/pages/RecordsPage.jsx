983f7f2f6df876f4bbd2b3a946ddca5fac3ba94e
yangzheng <yangzheng@qingting.fm>
2019-09-19 21:06:15 +0800

    fix(NoTicket): update column key

--- src/pages/RecordsPage.jsx
+++ src/pages/RecordsPage.jsx
@@ -311,7 +311,7 @@ class RecordsPage extends React.PureComponent {
         {...filterProps}
         title={column.getTableTitle(filterProps)}
         dataIndex={column.getKey()}
-        key={column.getKey()}
+        key={`${column.getKey()}${column.getTableTitle()}`}
         width={column.getTableWidth()}
         fixed={column.getTableFixed()}
         sorter={column.canSortInTable()}
