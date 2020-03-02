32cb29eb2fdba79dabe1cff767d5fb968736cca6
chenxiaoshan <chenxiaoshan@qingting.com>
2020-03-02 17:33:09 +0800

    fix(NoTicket): 由于支持 renderInFormItem ，Object的 formatFormFieldValue 应该是原始数据

--- src/schema/ObjectColumn.jsx
+++ src/schema/ObjectColumn.jsx
@@ -1,17 +1,9 @@
-import { isString, isPlainObject } from 'lodash';
+import { isString } from 'lodash';
 import React from 'react';
 import { Input } from 'antd';
 import Column from './Column';
 
 export default class ObjectColumn extends Column {
-  // eslint-disable-next-line class-methods-use-this
-  formatFormFieldValue(v) {
-    if (isPlainObject(v)) {
-      return JSON.stringify(v);
-    }
-    return v;
-  }
-
   // eslint-disable-next-line class-methods-use-this
   formatFormSubmitValue(v) {
     if (isString(v)) {
