1493b42d24289c982bc8b457e568bbaa4e5dcbf4
renxingyu <renxingyu@qingting.fm>
2020-09-30 11:37:07 +0800

    fix(NoTicket): export visiblePromise

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export Column from './schema';
 export Action from './actions';
 export request from './services/request';
 export history from './utils/history';
+export visiblePromise from './utils/visiblePromise';
 export Group from './components/Group';
 export ActivatorModal from './components/ActivatorModal';
 export Page from './pages/Page';
