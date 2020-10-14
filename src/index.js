c2f39e8fd9fee3bef4b92c8781cf5fe3d06d7404
yangzheng <yangzheng@qingting.fm>
2020-10-14 09:55:39 +0800

    feat(NoTicket): export all

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ export Page from './pages/Page';
 export DynamicPaginationTableTransfer from './components/Transfer/DynamicPaginationTableTransfer';
 export DraggableTable from './components/Table/DraggableTable';
 
-// export textToPath from './utils/textToPath';
+export textToPath from './utils/textToPath';
 export UploadImage from './components/Form/UploadImage';
 export UploadFile from './components/Form/UploadFile';
-// export ZoomImg from './components/ZoomImg';
-// export AudioPlayer from './components/Common/AudioPlayer';
+export ZoomImg from './components/ZoomImg';
+export AudioPlayer from './components/Common/AudioPlayer';
 export InputModal from './components/InputModal';
 
 // hooks
 export useUser from './hooks/useUser';
 export usePageData from './hooks/usePageData';
-// export useForm from './hooks/useForm';
+export useForm from './hooks/useForm';
